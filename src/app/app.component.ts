d2466c9265babf2de5bf43fe048f779eefd3510c
owner <foo@bar.com>
2018-04-03 01:18:42 +0200

    Create base app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Post} from "./post";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  title: string = 'Posts';
+  posts: Post[] = [
+    new Post('Premier post', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam congue vitae nunc pellentesque posuere.'),
+    new Post('Deuxième post', 'Nulla iaculis massa leo, at commodo velit rhoncus et. Phasellus nec odio ex.'),
+    new Post('Troisième post', 'Sed magna neque, tempor at sapien in, sodales porttitor enim.')
+  ];
 }
